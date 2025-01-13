f36c833ea23fc67d6bfa9af382a3f3062bc32321
Ana05509 <ana.salguero3883@utc.edu.ec>
2025-01-13 09:08:35 -0500

    s

--- Proyecto/Modelo/admin.py
+++ Proyecto/Modelo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Cliente, Proveedor, Articulos
+admin.site.register(Cliente)
+admin.site.register(Proveedor)
+admin.site.register(Articulos)
--- Proyecto/Modelo/models.py
+++ Proyecto/Modelo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(primary_key=True,max_length=10,unique=True,validators=[validacion_numeros,MinLengthValidator(10)])# Para validar que solo ingrese numeros no letras
+    direccion = models.TextField(max_length=50)
+    telefono = models.CharField(max_length=10)
+    email = models.EmailField()
+    def __str__(self):
+        return self.nombre
+
+class Proveedor(models.Model):
+    nombre = models.CharField(primary_key=True,max_length=10,unique=True)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=10)
+    email = models.EmailField()
+    def __str__(self):
+        return self.nombre
+    
+        
+class Articulos (models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    FechaElaboracion = models.IntegerField()
+    FechaVencimiento = models.IntegerField()
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+    
+
